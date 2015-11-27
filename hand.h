05d45c2e007c72ddf0b4aefbbf5bcd977332c6c9
mrbojangles72 <krockombergera@my.easternct.edu>
2015-11-27 15:37:01 -0500

    Update hand.h

--- hand.h
+++ hand.h
@@ -5,30 +5,23 @@ class hand{
 
 	public:
 
-		int distanceMoved;
-		int i,j;
+		finger fingers[4]; //each hand has 4 fingers that will type. Thumbs are for spacebar use only.
 
-		finger fingers[4];
-
-		int findClosestFinger(char myChar){ //returns the finger # that will type the next character.
+		//given a character, returns the finger # that will type the next character.
+		int findClosestFinger(char myChar){ 
 
 			int closestFinger;
 
-			for (i = 0; i<4; i++)
+			for (int i = 0; i<4; i++)
 			{
-				for (j = 0; j<8; j++)
+				for (int j = 0; j<8; j++)
 				{
 					if (fingers[i].territory[j] = myChar)
 					{
 						closestFinger = i;
-						i = 4;
-						j = 6;
 					}
 				}
 			}
-
-			i = 0;
-			j = 0;
 
 			return closestFinger;
 
