c453cbe65fec4e08932408b68445a51cd711c0cf
mrbojangles72 <krockombergera@my.easternct.edu>
2015-11-27 15:38:01 -0500

    Update finger.h

--- finger.h
+++ finger.h
@@ -1,30 +1,15 @@
 class finger{
 
 	public:
-		int positionX,positionY;
-		int lastPositionX,lastPositionY;
+		int positionX,positionY; // x and y positions of each finger
 
-		char territory[8];
-
-		int getPositionX()
-		{
-			return positionX;
-		}
-
-		int getPositionY()
-		{
-			return positionY;
-		}
-
-		void setLastPosition()
-		{
-			lastPositionX = positionX;
-			lastPositionY = positionY;
-		}
+		char territory[8]; //array of characters used to store the letters/symbols that a given finger 
+						   //will be responsible for typing.
 		
+		//set the (X,Y) position of a finger to (A,B)
 		void move(int a, int b)
 		{
-			positionX = a;
+			positionX = a;     
 			positionY = b;
 		}
 
